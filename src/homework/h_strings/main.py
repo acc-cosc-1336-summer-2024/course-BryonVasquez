7faf8222e77acac620921c576339acefd01861b4
BryonVasquez <143046378+BryonVasquez@users.noreply.github.com>
2024-07-06 14:34:01 +0000

    HW 5

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,41 @@
-#
+# 
+from strings import get_hamming_distance, get_dna_complement 
+
+def print_menu(): 
+    while True: 
+        try: 
+            choice = int(input("Enter your choice (1/2/3): ")) 
+            if choice in [1, 2, 3]: 
+                return choice 
+            else: 
+                print("Invalid choice. Please enter 1, 2, or 3.") 
+        except ValueError: 
+            print("Invalid input. Please enter a number.") 
+             
+
+
+def main(): 
+    while True: 
+        print_menu() 
+        choice == get_user_choice() 
+
+        if choice == 1: 
+            dna1 = input("Enter the first DNA sequence: ") 
+            dna2 = input("Enter the second DNA sequence: ") 
+            try: 
+                distance = get_hamming_distance(dna1, dna2) 
+                print (f"Hamming Distance between {dna1} and {dna2} is: {distance}")  
+            except ValueError as e: 
+                print(f"Error: {e}") 
+
+        
+        elif choice == 2: 
+            dna = input("Enter a DNA sequence: ") 
+            complement = get_dna_complement(dna) 
+            print(f"DNA complemnt of {dna} is: {complement}") 
+
+        elif choice == 3: 
+            print("Exiting program...") 
+            break 
+
+            
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,9 @@
-#
+# 
+def get_hamming_distance(dna1, dna2): 
+    if len(dna1) != len(dna2): 
+        raise ValueError("DNA sequences must be of equal length ") 
+    return sum(1 for x, y in zip(dna1, dna2) if x != y) 
+
+def get_dna_complement(dna): 
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'} 
+    return ''.join(complement[base] for base in dna)  
