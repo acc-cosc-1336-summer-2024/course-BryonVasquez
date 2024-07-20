ddd0be129c21bd8cc80586c858f63caa127f8157
BryonVasquez <143046378+BryonVasquez@users.noreply.github.com>
2024-07-20 17:39:47 +0000

    HW 7

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,17 @@
-#
+# 
+from class_a import die
+
+def main():
+    game_die = die()
+    
+    while True:
+        input("Press Enter to roll the die...")
+        game_die.roll()
+        print(game_die)
+        
+        choice = input("Roll again? (y/n): ").strip().lower()
+        if choice != 'y':
+            break
+
+if __name__ == "__main__":
+    main()
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,14 @@
-#
+# 
+
+import unittest 
+from src.homework.j_classes.class_a import Die 
+
+class TestDie(unittest.TestCase) 
+
+def test_get_rolled_value(self): 
+    die = Die() 
+    for _ in range(3): 
+        die.roll() 
+        rolled_value = die_get_rolled_value() 
+        self.assertGreaterEqual(rolled_value, 1, "Rolled value should bc >= 1") 
+        self.assertGreatEqual(rolled_vaule, 6, "Rolled value should bc >= 6") 
